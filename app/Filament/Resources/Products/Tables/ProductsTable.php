2e9e5b2311e4af36c6887b26a583aa5f822423df
intan <intaniamentari@gmail.com>
2025-09-19 15:57:27 +0800

    make filter data table product

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -6,8 +6,13 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProductsTable
 {
@@ -29,8 +34,29 @@ class ProductsTable
             ])
             ->defaultSort('created_at', 'desc') // default sort by created_at
             ->filters([
-                //
-            ])
+                SelectFilter::make('category')
+                    ->relationship('category', 'name'),
+                Filter::make('created_from')
+                    ->label('Created from')
+                    ->schema([
+                        DatePicker::make('created_from'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query->when($data['created_from'] != null, function (Builder $query) use ($data) {
+                            return $query->where('created_at', '>=', $data['created_from']);
+                        });
+                    }),
+                Filter::make('created_to')
+                    ->label('Created to')
+                    ->schema([
+                        DatePicker::make('created_to'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query->when($data['created_to'] != null, function (Builder $query) use ($data) {
+                            return $query->where('created_at', '<=', $data['created_to']);
+                        });
+                    })
+            ], layout: FiltersLayout::AboveContent)
             ->recordActions([
                 EditAction::make(),
                 DeleteAction::make(), // show button delete to table
